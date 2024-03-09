d26aaba9950563620e7130c460390cf8c51c5126
misaka8912 <misaka08912@gmail.com>
2024-03-09 23:30:55 +0800

    Site updated: 2024-03-09 23:30:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/230527/","posts/230705/","posts/230726/","posts/230718/","posts/231216/","posts/231213/","posts/240103/","posts/240106/","posts/240218/"];function toRandomPost(){
+var posts=["posts/230527/","posts/230705/","posts/230718/","posts/231216/","posts/231213/","posts/230726/","posts/240103/","posts/240106/","posts/240218/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
